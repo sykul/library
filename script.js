6f6e37b2ea3a3b68d57b4e173fd542f2a38541af
sykul <luke.flatley@outlook.com>
2024-01-01 18:59:52 +0100

    add event listener to button

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const myLibrary = [];
 
+function Book(title) {
+    this.title = title;
+}
+
+function addBookToLibrary() {
+    let userInput = document.getElementById('inputField').value;
+    alert(userInput);
+}
+
+const subButton = document.getElementById('subButton');
+subButton.addEventListener('click', addBookToLibrary, false)
