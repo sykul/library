3a451541ac412462eaa3c10428b0cdbc696f5774
sykul <luke.flatley@outlook.com>
2024-01-06 18:03:14 +0100

    append textbox value to object

--- script.js
+++ script.js
@@ -20,25 +20,31 @@ function addBookToArray() {
 }
 
 function displayBooks() {
-    for (book in myLibrary) {
-        console.log(myLibrary.length)
+    for (const book in myLibrary) {
+        console.log(book)
+        const newDiv = document.createElement('div');
+        const bookTitle = document.createTextNode(book);
+        newDiv.appendChild(bookTitle);
+        cardArea.appendChild(newDiv);
     }
 }
 
 const modal = document.getElementById('addBookModal');
 const modalButton = document.getElementsByClassName('modalButton')[0];
 const closeButton = document.getElementsByClassName("close")[0];
-const inputButton = document.getElementById('inputButton');
-const inputBox = document.getElementById('inputBox');
+const inputButton = document.querySelector('#inputButton');
+const inputBox = document.querySelector('.inputBox');
 const modalForm = document.querySelector('.modalForm');
+const cardArea = document.querySelector('.card-area')
 
 modalButton.addEventListener('click', openModal, false);
 closeButton.addEventListener('click', closeModal, false);
-inputButton.addEventListener('click', displayBooks, false);
-inputButton.addEventListener('click', closeModal, false);
+/* inputButton.addEventListener('click', displayBooks, false); */
+/* inputButton.addEventListener('click', closeModal, false); */
 modalForm.addEventListener('submit', function(event){
     event.preventDefault();
     addBookToArray();
+    displayBooks();
 })
 
 displayBooks()
